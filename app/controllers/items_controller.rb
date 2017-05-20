e039942d58971ba11a8d4a4ad0a372258b126015
David Paquette <paquettedavid@me.com>
2017-05-20 13:51:49 -0400

    Item Index page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   # GET /items
   # GET /items.json
   def index
-    @items = Item.all
+    @items = Item.all.where(availability:true)
   end
 
   # GET /items/1
